212837b10e6e6037d50325c8186ea33ccca06426
Aaron Cutress <accutress@gmail.com>
2025-04-24 20:13:25 +0800

    Remove broken test

--- tests/query_test.go
+++ tests/query_test.go
@@ -4,32 +4,7 @@ import (
 	"testing"
 
 	"github.com/aaroncutress/gtfs-go"
-	"github.com/aaroncutress/gtfs-go/models"
 )
-
-func TestGetIsTripRunningToday(t *testing.T) {
-	// Create a GTFS instance
-	g := &gtfs.GTFS{}
-	err := g.FromDB("test.db")
-	if err != nil {
-		t.Fatalf("Failed to load GTFS database: %v", err)
-	}
-
-	// Get a trip ID (replace with a valid trip ID from your GTFS data)
-	tripID := "2103211"
-
-	// Check if the trip is running today
-	isRunning, err := g.IsRunningToday(models.Key(tripID))
-	if err != nil {
-		t.Fatalf("Failed to check if trip is running today: %v", err)
-	}
-
-	if !isRunning {
-		t.Logf("Trip %s is not running today", tripID)
-	} else {
-		t.Logf("Trip %s is running today", tripID)
-	}
-}
 
 // Tests getting all current trips from the GTFS database
 func TestGetCurrentTrips(t *testing.T) {
